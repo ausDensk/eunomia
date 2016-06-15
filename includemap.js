f4af8dbe6431337eae437d01856b18f3217023f0
starrvinc <dshingis.qun@gmail.com>
2016-06-15 22:24:27 +0200

    Funktioniertende & halbwegs strukturierte Version von includemap

--- includemap.js
+++ includemap.js
@@ -2,47 +2,53 @@ console.log("Map wird initialisiert.");
 console.log(locationCoordinates);
 var markerArray = [];
 var windowArray = [];
+var map = drawMap();
 
+createArrayOfInfoWindows(locationCoordinates);
 locationCoordinates.forEach(function (element, j, arr) {
     if (element[4] == "publish") {
-        var marker = new google.maps.Marker({
-            position: {
-                lat: Number(element[0]),
-                lng: Number(element[1])
-            },
-            map: map
-        });
+        var marker = addMarker(Number(element[0]), Number(element[1]))
         markerArray.push(marker);
         marker.addListener("click", function () {
             console.log(windowArray + j);
+            console.log(windowArray)
             windowArray[j].open(map, marker);
         })
     }
 });
+
+function addMarker(mLat, mLng) {
+    return new google.maps.Marker({
+        position: {
+            lat: mLat,
+            lng: mLng
+        },
+        map: map
+    });
+}
 
 function drawMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: new google.maps.LatLng(51.4344079, 6.7623293),
     });
+    return map;
 };
 
 function createArrayOfInfoWindows(coordinates) {
     for (var i = 0; i < locationCoordinates.length; i++) {
-        var link = document.createElement("a");
-        link.href = locationCoordinates[i][2];
-        var postname = document.createTextNode(locationCoordinates[i][3]);
-        link.appendChild(postname);
+        var newLink = createNewLink(coordinates[i]);
         var infowindow = new google.maps.InfoWindow({
-            content: link
+            content: newLink
         });
         windowArray.push(infowindow);
     };
 }
 
-function createNewLink (coordDataSet) {
+function createNewLink(coordDataSet) {
     var newLink = document.createElement("a");
-    link.href = coordDataSet[2];
+    newLink.href = coordDataSet[2];
     var postname = document.createTextNode(coordDataSet[3]);
-    link.appendChild(postname);
+    newLink.appendChild(postname);
+    return newLink
 }
